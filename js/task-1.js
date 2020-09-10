df3cb1cda37bca6329876e1cc3f86db95e4c0b8a
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-09-10 00:30:31 -0700

    first task in task-1

---
+++ js/task-1.js
@@ -0,0 +1,5 @@
+const name = "Protective field generator";
+let price = 1000;
+console.log('Selected "', name, '", price per piece', price, "credits");
+price = 2000;
+console.log('Selected "', name, '", price per piece', price, "credits");
