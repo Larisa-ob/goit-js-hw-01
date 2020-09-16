19f3de11a2e6f5cb7386fb53cf0fb917ea3f9c96
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-09-16 16:58:20 -0700

    Task-1-6 with message

--- js/task-1-6.js
+++ js/task-1-6.js
@@ -2,7 +2,12 @@ let input;
 let total = 0;
 do {
   input = prompt("Enter the namber");
-  console.log(input);
-  total += Number(input);
+  const invalidNamber = Number(input);
+  if (Number.isNaN(invalidNamber)) {
+    alert(`No number is entered, please try again`);
+  } else {
+    console.log(input);
+    total += Number(input);
+  }
 } while (input !== null);
 alert(`The total amount is ${total}`);
